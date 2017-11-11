8db96b97ac0ea60de2cc15fa484a399d92b57295
张弓 <sz9214e@qq.com>
2017-11-11 08:04:48 +0800

    代码格式重排

--- src/main/java/cn/cerc/jdb/core/IConfig.java
+++ src/main/java/cn/cerc/jdb/core/IConfig.java
@@ -1,10 +1,11 @@
 package cn.cerc.jdb.core;
 
 public interface IConfig {
-    public String getProperty(String key, String def);
+	public String getProperty(String key, String def);
 
-    public String getProperty(String key);
-//    default public String getProperty(String key) {
-//        return this.getProperty(key, null);
-//    }
+	public String getProperty(String key);
+	
+	// default public String getProperty(String key) {
+	// return this.getProperty(key, null);
+	// }
 }
