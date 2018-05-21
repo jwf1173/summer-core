9a51bf4fb5cbf8b59f1e512849f2e2fd4dbbba98
ZhangGong <sz9214e@qq.com>
2018-05-21 23:13:43 +0800

    增加序列化与反序列化

--- src/main/java/cn/cerc/jdb/core/Utils.java
+++ src/main/java/cn/cerc/jdb/core/Utils.java
@@ -1,5 +1,11 @@
 package cn.cerc.jdb.core;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Utils {
     /**
      * 保障查询安全，防范注入攻击
@@ -11,4 +17,18 @@ public class Utils {
     public static String safeString(String value) {
         return value == null ? "" : value.replaceAll("'", "''");
     }
+
+    public static String serializeToString(Object obj) throws IOException {
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream objOut = new ObjectOutputStream(byteOut);
+        objOut.writeObject(obj);
+        return byteOut.toString("ISO-8859-1");// 此处只能是ISO-8859-1,但是不会影响中文使用;
+    }
+
+    public static Object deserializeToObject(String str) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(str.getBytes("ISO-8859-1"));
+        ObjectInputStream objIn = new ObjectInputStream(byteIn);
+        return objIn.readObject();
+    }
+
 }
