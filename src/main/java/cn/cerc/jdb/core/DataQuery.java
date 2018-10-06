75a0083f9f70645b24abc65946508070e57a65db
ZhangGong <sz9214e@qq.com>
2018-10-06 09:12:14 +0800

    应用SqlText

--- src/main/java/cn/cerc/jdb/core/DataQuery.java
+++ src/main/java/cn/cerc/jdb/core/DataQuery.java
@@ -1,17 +1,16 @@
 package cn.cerc.jdb.core;
 
-import java.util.ArrayList;
-
 public abstract class DataQuery extends DataSet {
     private static final long serialVersionUID = 7316772894058168187L;
     // 批次保存模式，默认为post与delete立即保存
     private boolean batchSave = false;
-    protected String commandText;
+    protected SqlText commandText;
     protected boolean active = false;
     protected IHandle handle;
 
     public DataQuery(IHandle handle) {
         this.handle = handle;
+        commandText = new SqlText();
     }
 
     // 打开数据集
@@ -40,38 +39,17 @@ public abstract class DataQuery extends DataSet {
      * @return 返回对象本身
      */
     public DataQuery add(String sql) {
-        if (commandText == null)
-            commandText = sql;
-        else
-            commandText = commandText + " " + sql;
+        commandText.add(sql);
         return this;
     }
 
     public DataQuery add(String format, Object... args) {
-        ArrayList<Object> items = new ArrayList<>();
-        for (Object arg : args) {
-            if (arg instanceof String) {
-                items.add(Utils.safeString((String) arg));
-            } else {
-                items.add(arg);
-            }
-        }
-        return this.add(String.format(format, items.toArray()));
-    }
-
-    /**
-     * 设置要执行的sql指令（已停用，请改使用add函数，以防止注入攻击）
-     * 
-     * @param sql
-     *            要执行的sql指令
-     */
-    @Deprecated
-    public void setCommandText(String sql) {
-        this.commandText = sql;
+        commandText.add(format, args);
+        return this;
     }
 
     public String getCommandText() {
-        return this.commandText;
+        return this.commandText.getSelect();
     }
 
     /**
@@ -80,7 +58,7 @@ public abstract class DataQuery extends DataSet {
      * @return 返回自身
      */
     public DataQuery emptyCommand() {
-        this.commandText = null;
+        this.commandText.clear();
         return this;
     }
 
