6941d472341cbed819f3f7a3b23e4d4e0882527e
黄荣君 <l1091462907@gmail.com>
2018-10-08 14:23:48 +0800

    Revert "应用SqlText"
    This reverts commit 75a0083f9f70645b24abc65946508070e57a65db.

--- src/main/java/cn/cerc/jdb/core/DataQuery.java
+++ src/main/java/cn/cerc/jdb/core/DataQuery.java
@@ -1,16 +1,17 @@
 package cn.cerc.jdb.core;
 
+import java.util.ArrayList;
+
 public abstract class DataQuery extends DataSet {
     private static final long serialVersionUID = 7316772894058168187L;
     // 批次保存模式，默认为post与delete立即保存
     private boolean batchSave = false;
-    protected SqlText commandText;
+    protected String commandText;
     protected boolean active = false;
     protected IHandle handle;
 
     public DataQuery(IHandle handle) {
         this.handle = handle;
-        commandText = new SqlText();
     }
 
     // 打开数据集
@@ -39,17 +40,38 @@ public abstract class DataQuery extends DataSet {
      * @return 返回对象本身
      */
     public DataQuery add(String sql) {
-        commandText.add(sql);
+        if (commandText == null)
+            commandText = sql;
+        else
+            commandText = commandText + " " + sql;
         return this;
     }
 
     public DataQuery add(String format, Object... args) {
-        commandText.add(format, args);
-        return this;
+        ArrayList<Object> items = new ArrayList<>();
+        for (Object arg : args) {
+            if (arg instanceof String) {
+                items.add(Utils.safeString((String) arg));
+            } else {
+                items.add(arg);
+            }
+        }
+        return this.add(String.format(format, items.toArray()));
+    }
+
+    /**
+     * 设置要执行的sql指令（已停用，请改使用add函数，以防止注入攻击）
+     * 
+     * @param sql
+     *            要执行的sql指令
+     */
+    @Deprecated
+    public void setCommandText(String sql) {
+        this.commandText = sql;
     }
 
     public String getCommandText() {
-        return this.commandText.getSelect();
+        return this.commandText;
     }
 
     /**
@@ -58,7 +80,7 @@ public abstract class DataQuery extends DataSet {
      * @return 返回自身
      */
     public DataQuery emptyCommand() {
-        this.commandText.clear();
+        this.commandText = null;
         return this;
     }
 
